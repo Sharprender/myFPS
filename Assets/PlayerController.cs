5f2d4ffd132cd01882fdc22abfce16b6ff6a4a6e
Sharprender <sgtsilver492@hotmail.com>
2017-11-26 13:44:37 +0100

    PlayerContrller and PlayerMotor added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 500000f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+    }
+}
